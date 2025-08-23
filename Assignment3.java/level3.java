f9090fe5b3aa499830ff6afd738f67c75c5cdc66
Gauri Gaur <gaurigaur234@gmail.com>
2025-08-23 23:49:10 +0530

    second commit

--- Assignment3.java/level3.java
+++ Assignment3.java/level3.java
@@ -5,7 +5,7 @@ public class level3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
-
+//next
         do {
             System.out.print("Enter ques number: ");
             choice = sc.nextInt();
