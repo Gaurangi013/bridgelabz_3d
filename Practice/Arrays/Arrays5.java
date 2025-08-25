4f4a6e6f88ed60abfa10de2a2ca69b982879f7ed
Gauri Gaur <gaurigaur234@gmail.com>
2025-08-25 11:16:14 +0530

    practice

---
+++ Practice/Arrays/Arrays5.java
@@ -0,0 +1,18 @@
+public class Arrays5 {
+    // 5.Check if an array is sorted in ascending order.
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        boolean isSorted = true;
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The array is sorted in ascending order.");
+        } else {
+            System.out.println("The array is not sorted in ascending order.");
+        }
+    }
+}
