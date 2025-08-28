a1decc860bc0062d38ad22819ec06f1725bd6d93
Gauri Gaur <gaurigaur234@gmail.com>
2025-08-28 16:42:28 +0530

    string

---
+++ strings/level1/ques1.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class ques1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s1 = sc.next();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.next();
+
+        boolean same = true;
+        if (s1.length() != s2.length()) {
+            same = false;
+        } else {
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    same = false;
+                    break;
+                }
+            }
+        }
+
+        boolean equalsResult = s1.equals(s2);
+
+        System.out.println("Manual check (charAt): " + same);
+        System.out.println("Built-in equals(): " + equalsResult);
+
+        sc.close();
+    }
+}
